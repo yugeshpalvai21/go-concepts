82f8f137701dfcf3f1c16b56cedb7c054fca60e6
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-12-05 17:18:47 +0530

    Go DataTypes: Strings

---
+++ 02_strings/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// first way to define explicitly
+	var name string = "yugesh"
+
+	// second way to define without type
+	var location = "IND"
+
+	// third way to define dynamically
+	course := "CSE"
+
+	fmt.Println(name, location, course)
+}
