df64b6d22c506597c029e845434ba1d38289ee73
Yugesh Palvai <yugeshpalvai21@gmail.com>
2023-09-09 20:12:28 +0530

    generates go mod init to avoid maintain workspace

--- 02_strings/main.go
+++ 02_strings/main.go
@@ -8,23 +8,22 @@ import (
 func main() {
 	// traditional way of declaration
 	var first_name string = "Yugesh"
-	fmt.Println("First Name", first_name)
+	fmt.Println("First Name:", first_name)
 
 	// shorthand syntax
 	last_name := "Palvai"
-	fmt.Println(last_name)
+	fmt.Println("last name:", last_name)
 
 	// length of the string
-	fmt.Println(len(first_name))
+	fmt.Println("Number of Chars", len(first_name))
 
 	// convert string to uppercase, lowercase and capitalize
 	fmt.Println(strings.ToUpper(first_name))
 	fmt.Println(strings.ToLower(last_name))
-	fmt.Println(strings.Title(firs_name))
+	fmt.Println(strings.Title(first_name))
 
 	// string concatenation
 
 	full_name := first_name + " " + last_name
 	fmt.Println(full_name)
-
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_concepts
+
+go 1.21.1
