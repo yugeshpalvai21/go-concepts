c19fcb2c6e3f9dbe6a7d6f1fe7c6d6ac093ae923
Yugesh Palvai <yugeshpalvai21@gmail.com>
2023-09-19 00:25:03 +0530

    GO Structs - constructors

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -4,32 +4,35 @@ import (
 	"fmt"
 )
 
-// Unlike other languages(Ruby, JS etc..) there is no class concept in Go,
-// instead we can use stucts to create instances and stuff
-
 // Defining struct
 type Person struct {
-	FirstName string
-	LastName  string
-	Location  string
+	Name string
+	Age  int
 }
 
-// add function to above Person struct => this is like public method available on Person object
+// create pointer constrictor
+func NewPerson(name string, age int) *Person {
+	return &Person{Name: name, Age: age}
+}
 
-func (p Person) getFullName() string {
-	return p.FirstName + " " + p.LastName
+// add public method to struct
+func (p Person) getDetails() string {
+	result := fmt.Sprintf("Name: %s, and Age: %d", p.Name, p.Age)
+	return result
 }
 
 func main() {
-	// instantiating struct
-	person1 := Person{
-		FirstName: "Yugesh",
-		LastName:  "Palvai",
-	}
+	//instantiating struct
+	person1 := Person{}
 
-	person1.Location = "IND"
+	fmt.Println("person object", person1)
+	fmt.Println("person name >", person1.Name)
+	// change person1 age
+	person1.Age = 29
+	fmt.Println("person age >", person1.Age)
+	fmt.Println("person memory address", &person1)
 
-	fmt.Println("Person object", person1)
-	fmt.Println("Person1 firstName", person1.FirstName)
-	fmt.Println("Person1 full name is - ", person1.getFullName())
+	person2 := NewPerson("Raam", 30)
+	fmt.Println("person2 object...", person2)
+	fmt.Println("person2 details...", person2.getDetails())
 }
