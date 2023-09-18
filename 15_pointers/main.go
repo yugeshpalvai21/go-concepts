6b045ce81a580737bb6c49f6019ecab8f450db80
Yugesh Palvai <yugeshpalvai21@gmail.com>
2023-09-18 23:39:14 +0530

    Go DataTypes - Pointers

--- 15_pointers/main.go
+++ 15_pointers/main.go
@@ -5,18 +5,18 @@ import (
 )
 
 func main() {
-	// pointers in Go are just variables that stores memory address of another variable
+	var ptr *int // defining variable that stores memory address of other integer variable
 
-	var ptr *int
-	x := 10
-
+	x := 5
 	ptr = &x
 
-	fmt.Println("memorey address that pointer holds", ptr)
-	fmt.Println("printing values of x and pointer", x, *ptr)
+	fmt.Println("Value of x variable -", x)
+	fmt.Println("Value of ptr variable -", ptr)
+	fmt.Println("Value of memory address which stored in ptr variable", *ptr)
 
-	// if we change the value of pointer that changes the value of x too..
+	// if i change the value of memory address that stored in ptr variable
 
-	*ptr = 20
-	fmt.Println("printing values of x and pointer", x, *ptr)
+	*ptr = 15
+	fmt.Println("Updated Value of x variable -", x)
+	fmt.Println("Updated Value of memory address which stored in ptr variable", *ptr)
 }
